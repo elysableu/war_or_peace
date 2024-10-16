a16bb6c1c7d3c5a70932f1f32afa1487df89e973
elysableu <elysabward@gmail.com>
2024-10-16 11:00:14 -0600

    Removed duplicate from Deck class

--- lib/deck.rb
+++ lib/deck.rb
@@ -18,7 +18,7 @@ class Deck
   def rank_of_card_at(card_index)
     card = @cards[card_index]
     card.rank
-    binding.pry
+    # binding.pry
   end
 
   # Return an array of cards in the deck that
@@ -36,54 +36,6 @@ class Deck
   # Return percentage of cards that are high ranking
   def percent_high_ranking
     (high_ranking_cards.length.to_f / count * 100).round(2)
-  end
-
-  # Remove top card from the deck
-  def remove_card
-    @cards.shift()
-    @cards
-  end
-
-  # Add one card to bottom of the deck 
-  def add_card(card)
-    @cards << card
-  end
-end
-
-class Deck
-  attr_reader :cards
-
-  def initialize(cards)
-    @cards = cards
-    @count = 0
-  end
-
-  # Returns the number of cards in deck
-  def count
-    @count = @cards.length
-  end
-
-  # takes index location of card to 
-  # be used and will return rank of that card
-  def rank_of_card_at(card_index)
-    @cards[card_index].rank
-  end
-
-  # Return an array of cards in the deck that
-  # have rank 11 or above
-  def high_ranking_cards
-    high_ranking = []
-    @cards.each do |card|
-      if card.rank >= 11
-        high_ranking << card
-      end
-    end
-    high_ranking
-  end
-
-  # Return percentage of cards that are high ranking
-  def percent_high_ranking
-    (high_ranking_cards.length.to_f / @cards.length * 100).round(2)
   end
 
   # Remove top card from the deck
